aa1c0ea3042b8aa9b18371de7d91eb09cf9e1c09
Iury Chagas <iurychagas@icloud.com>
2020-03-03 15:02:42 -0300

    add Update Models

---
+++ Migrations/20200303175823_addbooks.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoRead.Migrations
+{
+    public partial class addbooks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    PageQuantity = table.Column<int>(nullable: false),
+                    Image = table.Column<string>(nullable: true),
+                    Publisher = table.Column<string>(nullable: true),
+                    Language = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
--- Models/Book.cs
+++ Models/Book.cs
@@ -10,11 +10,8 @@ namespace ToDoRead.Models
         public decimal Price { get; set; }
         public int PageQuantity { get; set; }
         public string Image { get; set; }
-        public DateTime CreateDate { get; set; }
-        public DateTime LastUpdateDate { get; set; }
         public string Publisher { get; set; }
         public string Language { get; set; }
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
+
     }
 }
