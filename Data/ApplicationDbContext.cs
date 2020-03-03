419aea18eb43c2d8c9c842934e81b6191d79a973
Iury Chagas <iurychagas@icloud.com>
2020-03-03 13:59:52 -0300

    add database context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using ToDoRead.Models;
 
 namespace ToDoRead.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Book> Books { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
